f87f8046351627da40cda5179994165bb110b1d3
Stking89 <stking89@icloud.com>
2024-01-02 14:11:54 -0600

    updated pr trigger

--- docker.sh
+++ docker.sh
@@ -13,6 +13,9 @@ gcloud auth configure-docker ${ARTIFACT_REGISTRY}
 # Pull the source image
 docker pull ${SOURCE_IMAGE}
 
+# Install bash if not already present
+docker run -it alpine apk add --no-cache bash
+
 # Tag the image for Artifact Registry
 docker tag ${SOURCE_IMAGE} ${ARTIFACT_REGISTRY}/${PROJECT_ID}/${REPOSITORY_NAME}/${IMAGE_NAME}
 
