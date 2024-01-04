26bdacd1ec401a8c91b6077fd0a7d9570e09b458
Stking89 <stking89@icloud.com>
2024-01-04 13:09:44 -0600

    adding deploy trigger

--- modules/infrastructure/cloudbuild_triggers.tf
+++ modules/infrastructure/cloudbuild_triggers.tf
@@ -1,3 +1,9 @@
+#------------------------------------------------------------------------------------------------------------------------
+#                                                CLOUD BUILD TRIGGERS                             
+#------------------------------------------------------------------------------------------------------------------------
+
+#---PULL-REQUEST--- 
+
 resource "google_cloudbuild_trigger" "pr_trigger" {
   project  = var.project_id
   name     = "pr-trigger" 
@@ -19,5 +25,33 @@ resource "google_cloudbuild_trigger" "pr_trigger" {
 
   # Additional options (optional)
   filename = "modules/infrastructure/build_triggers/pull-request.yaml"  // Name of the build configuration file within the repository
+
+}
+
+#------------------------------------------------------------------------------------------------------------------------
+
+#---DEPLOY/MERGE---
+
+resource "google_cloudbuild_trigger" "deploy-trigger" {
+  project  = var.project_id
+  name     = "deploy-trigger" 
+  location = "us-central1"
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_UNSPECIFIED"
+
+  # Define the trigger configuration
+  github {
+    owner = "Stking89"
+    name  = "google_sandbox"
+    push {
+      branch = "main"  // Trigger on pushes to the main branch
+    }
+  }
+
+  # Specify the build configuration (optional)
+  included_files = ["modules/infrastructure/build_triggers/deploy.yaml"]  // Path to your Cloud Build configuration file
+
+  # Additional options (optional)
+  filename = "modules/infrastructure/build_triggers/deploy.yaml"  // Name of the build configuration file within the repository
 
 }
